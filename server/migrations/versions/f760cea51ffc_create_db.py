00663793e82cc6cd5bc9cdd2261f752075885775
George <georgefbrockman@gmail.com>
2024-07-12 21:51:11 -0500

    creates models

---
+++ server/migrations/versions/f760cea51ffc_create_db.py
@@ -0,0 +1,24 @@
+"""create db
+
+Revision ID: f760cea51ffc
+Revises: 
+Create Date: 2024-07-12 21:17:32.106526
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f760cea51ffc'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -4,3 +4,24 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = "customers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    number = db.Column(db.String)
+
+class Item(db.Model, SerializerMixin):
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    cost = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+
+class Transaction(db.Model, SerializerMixin):
+    __tablename__ = "transactions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime)
