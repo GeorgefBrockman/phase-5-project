d31420aebedb80375c5ad1857a327307c308fce5
Sasha <georgefbrockman@gmail.com>
2024-07-13 12:45:55 -0500

    adds associations between tables in models

--- server/models.py
+++ server/models.py
@@ -12,6 +12,10 @@ class Customer(db.Model, SerializerMixin):
     email = db.Column(db.String)
     number = db.Column(db.String)
 
+    transactions = db.relationship('Transaction', back_populates='customers')
+
+    items = association_proxy('transactions', 'item', creator=lambda item_obj: Transaction(item=item_obj))
+
 class Item(db.Model, SerializerMixin):
     __tablename__ = "items"
 
@@ -20,8 +24,16 @@ class Item(db.Model, SerializerMixin):
     cost = db.Column(db.Float)
     quantity = db.Column(db.Integer)
 
+    transactions = db.relationship('Transaction', back_populates='items')
+
 class Transaction(db.Model, SerializerMixin):
     __tablename__ = "transactions"
 
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime)
+
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+
+    items = db.relationship('Item', back_populates="transactions")
+    customers = db.relationship('Customer', back_populates="transactions")
