314f15b1231f15fc0fbbc9f23e22f1c1ffb05dfe
Sasha <georgefbrockman@gmail.com>
2024-07-13 22:54:37 -0500

    adds functionality to customer views

--- server/app.py
+++ server/app.py
@@ -20,11 +20,97 @@ def index():
 
 @app.route('/customers', methods=['GET', 'POST'])
 def customers():
-    pass
+    
+    if request.method == 'GET':
+        customers = []
+        for customer in Customer.query.all():
+            customer_dict = customer.to_dict()
+            customers.append(customer_dict)
+
+        response = make_response(
+            customers,
+            200
+        )
+
+        return response
+
+    elif request.method == 'POST':
+        new_customer = Customer(
+            name = request.form.get('name'),
+            email = request.form.get('email'),
+            number = request.form.get('number'),
+        )
+
+        db.session.add(new_customer)
+        db.session.commit()
+
+        customer_dict = new_customer.to_dict()
+
+        response = make_response(
+            customer_dict,
+            201
+        )
+
+        return response
 
 @app.route('/customers/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
-def customer_by_id():
-    pass
+def customer_by_id(id):
+    customer = Customer.query.filter(Customer.id == id).first()
+
+    if customer == None:
+        response_body = {
+            'message': 'This customer does not exist in our database. Please try again'
+        }
+
+        response = make_response(
+            response_body,
+            404
+        )
+
+        return response
+
+    else:
+        if request.method == 'GET':
+            customer_dict = customer.to_dict()
+
+            response = make_response(
+                customer_dict,
+                200
+            )
+
+            return response
+
+        elif request.method == 'PATCH':
+            for attr in request.form:
+                setattr(customer, attr, request.form.get(atr))
+
+            db.session.add(customer)
+            db.session.commit()
+
+            customer_dict = customer.to_dict()
+
+            response = make_response(
+                customer_dict,
+                200
+            )
+
+            return response
+
+        elif request.method == 'DELETE':
+            db.session.delete(customer)
+            db.session.commit()
+
+            response_body = {
+                'delete_successful': True,
+                'message': 'Review deleted'
+            }
+
+            response = make_response(
+                response_body,
+                200
+            )
+
+            return response
 
 @app.route('/transactions', methods=['GET', 'POST'])
 def transactions():
