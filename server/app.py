deb9fdc12a384dcfd7e9712f205374594cdbd748
Sasha Brockman <130719691+SashaBrockman@users.noreply.github.com>
2024-07-15 11:59:57 -0500

    adds error handler

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@
 # Remote library imports
 from flask import request
 from flask_restful import Resource
+from werkzeug.exceptions import NotFound
 
 # Local imports
 from config import app, db, api
@@ -248,6 +249,18 @@ def inventory_by_id(id):
             )
 
             return response
+
+@app.errorhandler(NotFound)
+def handle_not_found(e):
+
+    response = make_response(
+        "Not Found: The requested resource does not exist.",
+        404
+    )
+
+    return response
+
+app.register_error_handler(404, handle_not_found)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
