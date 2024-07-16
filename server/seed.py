d753b6011b98c50407cf69de745ddd785d840cd9
Sasha Brockman <130719691+SashaBrockman@users.noreply.github.com>
2024-07-16 15:50:15 -0500

    fixes bug

--- server/seed.py
+++ server/seed.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
             customer = Customer(
                 name = fake.name(), 
                 email = fake.email(), 
-                number = fake.phone_number()
+                number = fake.numerify(text = "%#########")
             )
 
             customers.append(customer)
