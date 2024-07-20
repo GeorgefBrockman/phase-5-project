413836c2fb07de0196799c599d6d19d5aca0024f
Sasha <georgefbrockman@gmail.com>
2024-07-20 11:45:18 -0500

    adds functionality to NavBar component

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+function NavBar(){
+    return(
+        <nav className="nav-bar">
+            <NavLink to='/' className={'nav-link'}>Home</NavLink>
+            <NavLink to='/customers' className={'nav-link'}>Customers</NavLink>
+            <NavLink to='/transactions' className={'nav-link'}>Transactions</NavLink>
+            <NavLink to='/inventory' className={'nav-link'}>Inventory</NavLink>
+        </nav>
+    )
+}
+
+export default NavBar;
