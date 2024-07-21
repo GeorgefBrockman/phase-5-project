f8356dba0bfd7d22f7309836ff2b6896e9f8251c
Sasha <georgefbrockman@gmail.com>
2024-07-21 16:07:11 -0500

    adds delete button and functionality

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { StoreContext } from "./StoreContext";
 
 function Customer({customer}){
+    const {customers, setCustomers} = useContext(StoreContext)
+
     if(typeof customer == 'undefined') return(<h2>Loading...</h2>);
+
+    function handleDelete(e){
+        fetch(`/customers/${customer.id}`, {
+            method: "DELETE"
+        })
+        .then(r => r.json())
+        .then(r => {
+            if(r.delete_successful == true){
+                const newCustomers = customers.filter((cust) => cust != customer)
+                setCustomers(newCustomers)
+            }
+        })
+    }
     
     return(
         <li>
             <p style={{gridColumn: 1}}>{customer.name}</p>
             <p style={{gridColumn: 2}}>{customer.email}</p>
             <p style={{gridColumn: 3}}>{customer.number}</p>
+            <button onClick={handleDelete}>Delete</button>
         </li>
     )
 }
