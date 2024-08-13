64d7e29deddb35664770d70e6cd90f637addbc1d
Sasha <georgefbrockman@gmail.com>
2024-08-12 23:29:22 -0500

    adds employee id to transaction form

--- client/src/components/NewTransaction.js
+++ client/src/components/NewTransaction.js
@@ -4,22 +4,26 @@ import * as yup from "yup";
 import { StoreContext } from "./StoreContext";
 
 function NewTransaction(){
-    const {setTransactions, customers, transactions} = useContext(StoreContext)
+    const {setTransactions, customers, transactions, employees, setEmployees, inventory} = useContext(StoreContext)
 
     const formSchema = yup.object().shape({
         item_id: yup.string().required("Must enter an item id"),
-        number: yup.string().required("Must enter a phone number").max(10)
+        number: yup.string().required("Must enter a phone number").max(10),
+        employee_id: yup.string().required("Must enter an employee id")
     })
 
     const formik = useFormik({
         initialValues: {
             item_id: "",
             number: "",
+            employee_id: "",
         },
         validationSchema: formSchema,
         onSubmit: (values) => {
             const customerID = customers.find((customer) => customer.number === values.number).id
             values.item_id = parseInt(values.item_id)
+            values.employee_id = parseInt(values.employee_id)
+            const value = inventory.find((item) => item.id === values.item_id).cost
 
             const newValues = {
                 item_id: values.item_id,
@@ -38,6 +42,23 @@ function NewTransaction(){
                 const newTransactions = [...transactions, r];
                 setTransactions(newTransactions)
             })
+
+            fetch(`/employees/${values.employee_id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({value_sold: value})
+            })
+            .then(r => {
+                if(r.status === 200){
+                    fetch("/employees")
+                    .then(r => r.json())
+                    .then( r => {
+                        setEmployees(r);
+                    })
+                }
+            })
         }
     })
 
@@ -48,9 +69,13 @@ function NewTransaction(){
             <input id="item_id" name="item_id" onChange={formik.handleChange} value={formik.values.item_id}/>
             <p style={{color: "red"}}>{formik.errors.item_id}</p>
 
-            <label htmlFor="number">Phone Number: </label>
+            <label htmlFor="number">Customer Phone: </label>
             <input id="number" name="number" onChange={formik.handleChange} value={formik.values.number}/>
             <p style={{color: "red"}}>{formik.errors.number}</p>
+
+            <label htmlFor="employee_id">Employee ID: </label>
+            <input id="employee_id" name="employee_id" onChange={formik.handleChange} value={(formik.values.employee_id)}/>
+            <p style={{color: "red"}}>{formik.errors.employee_id}</p>
 
             <button type="submit">Submit</button>
         </form>
